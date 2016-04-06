db20339ee78a48873abb3f38020266f1520675d1
aerii <aerii@live.com>
2016-04-06 00:29:35 -0700

    working functionality

---
+++ models.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var MessageSchema = new Schema({
+	email: String,
+	content: String,
+	created: Date
+});
+
+exports.Message = mongoose.model('Message', MessageSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,16 @@
+var models = require('../models');
+
 exports.view = function(req, res) {
-    var data = {data: []};
-    res.render("index", data);
+    models.Message
+    	.find()
+    	.sort('-created')
+    	.exec(displayPosts);
+
+    function displayPosts(err, msg_posts) {
+    	if (err) { console.log(err); res.send(500); }
+    	else {
+    		var data = {"data": msg_posts}
+    		res.render("index", data);
+    	}
+    }
 }
